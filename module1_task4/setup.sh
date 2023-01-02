315666d05be2334fb40863929ca4fbb986597456
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2023-01-02 18:56:31 +0000

    Fix setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 apt-get update && apt-get install -y git make curl build-essential
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
