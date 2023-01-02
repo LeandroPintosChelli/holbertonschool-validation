14c51d2bfaab2393af96fc13feaeb46675ee7aac
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2023-01-02 19:00:30 +0000

    Fix setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 apt-get update && apt-get install -y git make curl build-essential
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install hugo
