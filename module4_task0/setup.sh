30acd8f42b8787d5dda4f7e414ffa3c0e0b0112f
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2023-01-11 12:01:56 -0300

    Add task 0

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-apt-get update && apt-get install -y shellcheck snapd git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb && npm install -g markdownlint-cli
-sudo rm -rf /usr/bin/go
+sudo docker pull golang
+sudo docker run golang
+sudo docker exec -it golang curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+sudo docker exec -it golang sudo apt install ./hugo.deb
+sudo docker exec -it golang sudo rm hugo.deb
+sudo docker exec -it golang make build
