b47979be9f5c18bab522217c942e9a7a972e0585
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2023-01-11 10:29:39 -0300

    Fix files

--- module3_task0/setup.sh
+++ module3_task0/setup.sh
@@ -1,5 +1,5 @@
-sudo apt-get update && sudo apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
-dpkg -i hugo_0.79.0_Linux-64bit.deb
+#!/bin/bash
+apt-get update && apt-get install -y make golang-go git curl
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
 make build
-exit 0
